dc420c91ad75175d14dd1a71d6f7ca19752200d0
Your Name <you@example.com>
2024-05-06 14:02:34 +0300

    Complete code-challenge

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       <Routes>
         <Route
         
-          path="/r"
+          path="/phase-2-week-2-code-challenge/"
           element={
             <Botcolletion
               setBotspecsShown={setBotspecsShown}
